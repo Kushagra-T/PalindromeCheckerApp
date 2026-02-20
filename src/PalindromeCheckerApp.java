2bf21e6c47ff33d60a3d490f787070c261c42fab
Kushagra-T <kushagrasty.com>
2026-02-20 09:57:59 +0530

    UC1:Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,9 @@
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp
+{
+    public static void main(String[] args) {
+        double version=1.0;
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : " + version);
+        System.out.println("System initialized successfully.");
+    }
 }
