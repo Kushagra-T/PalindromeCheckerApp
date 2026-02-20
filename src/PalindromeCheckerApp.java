d8f6337a5e9a7f58207286f971c048e28bcf8c46
Kushagra-T <kushagrasty.com>
2026-02-20 11:46:06 +0530

    UC6: Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,13 +2,18 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text:");
         String word = sc.nextLine();
         boolean isPalindrome = true;
+
+
         char[] charArray = word.toCharArray();
         int start = 0;
         int end = charArray.length - 1;
+
+
         while (start < end) {
             if (charArray[start] != charArray[end]) {
                 isPalindrome = false;
@@ -17,6 +22,8 @@ public class PalindromeCheckerApp {
             start++;
             end--;
         }
+
+
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
