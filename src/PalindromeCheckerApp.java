6f2b1243c9dcda051e44801aab25c48d983b78fa
Kushagra-T <kushagrasty.com>
2026-02-20 10:38:54 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,23 @@
+import java.util.*;
 public class PalindromeCheckerApp
 {
     public static void main(String[] args) {
-        double version=1.0;
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : " + version);
-        System.out.println("System initialized successfully.");
+//        double version=1.0;
+//        System.out.println("Welcome to Palindrome Checker Management System");
+//        System.out.println("Version : " + version);
+//        System.out.println("System initialized successfully.");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input text:");
+        String word=sc.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < word.length() / 2; i++)
+        {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i))
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is it a Palindrome? : " +isPalindrome);
     }
 }
